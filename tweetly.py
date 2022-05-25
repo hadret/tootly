ad7d8245fb7e9749fe5f0d3b9aa9c36f3bd655f0
Filip Chabik <filip.chabik@samedi.de>
2022-05-25 14:40:27 +0200

    feat: initial tweetly.py structure.

---
+++ tweetly.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+from datetime import date, datetime
+import requests
+
+
+today = date.today()
+
+
+def parse_feed():
+    '''Fetches latest post from the RSS feed.'''
+    feed = requests.get('https://chabik.com/feed.xml')
+
+    if feed:
+        soup = BeautifulSoup(feed.text, 'xml')
+        latest_post = soup.find_all('item')[0]
+        post_title = latest_post.find('title').text
+        post_link = latest_post.find('link').text
+        post_date = latest_post.find('pubDate').text
+        post_date = datetime.strptime(post_date[:-6], "%a, %d %b %Y %H:%M:%S").date()
+        return post_title, post_date, post_link
+    else:
+        raise SystemExit("Feed couldn't be reached!")
+
+
+parse_feed()
