62bd4f2b832eecc1d9dc058c5f80c36df997c01d
Filip Chabik <hadret@gmail.com>
2026-04-06 09:57:40 +0200

    Bumped up to Pydantic v2

--- tootly/config.py
+++ tootly/config.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -13,8 +13,7 @@ class Settings(BaseSettings):
     mastodon_token: str = ""
     tags: list = []
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 @lru_cache
